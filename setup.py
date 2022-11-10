a5a4542bbfebffe8b1dbb9484393ebebd4507fd9
vibhatha <vibhatha@gmail.com>
2022-11-10 10:23:53 +0530

    feat(initial): setup testing

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+setup(
+    name='substrait_consumer',
+    version='0.0.1',
+    author='Substrait',
+    description='A Substrait consumer test suite',
+    long_description='Testing Substrait consumers with standard data processing queries',
+    url='https://github.com/substrait-io/consumer-testing',
+    keywords='substrait, consumer',
+    python_requires='>=3.9, <4',
+    packages=find_packages(include=['substrait_consumer', 'substrait_consumer.*']),
+    install_requires=[
+        'duckdb',
+        'filelock',
+        'ibis-framework',
+        'ibis-substrait',
+        'protobuf',
+        'pyarrow',
+        'pytest',
+        'pytest-xdist',
+        'substrait-validator',
+    ],
+    dependency_links=[
+      '--extra-index-url https://pypi.fury.io/arrow-nightlies --prefer-binary --pre'
+    ],
+    package_data={
+        'tests': ['tests/integration'],
+    }
+)
