431111c286dbbdaab48d46be174da2c9c6b1bcdb
Richard Tia <richie.tia@gmail.com>
2022-11-10 11:52:26 -0800

    refactor: separate out common method in produers

--- tests/producers.py
+++ tests/producers.py
@@ -44,38 +44,9 @@ class DuckDBProducer:
         proto_bytes = duckdb_substrait_plan.fetchone()[0]
         return proto_bytes
 
-    def load_tables_from_parquet(
-        self,
-        created_tables: set,
-        file_names: Iterable[str],
-    ) -> list:
-        """
-        Load all the parquet files into separate tables in DuckDB.
-
-        Parameters:
-            created_tables:
-                The set of tables that have already been created.
-            file_names:
-                Name of parquet files.
-        Returns:
-            A list of the table names.
-        """
-        parquet_file_paths = SubstraitUtils.get_full_path(file_names)
-        table_names = []
-        for file_name, file_path in zip(file_names, parquet_file_paths):
-            table_name = Path(file_name).stem
-            table_name = table_name.translate(str.maketrans("", "", string.punctuation))
-            if table_name not in created_tables:
-                create_table_sql = f"CREATE TABLE {table_name} AS SELECT * FROM read_parquet('{file_path}');"
-                self.db_connection.execute(create_table_sql)
-                created_tables.add(table_name)
-            table_names.append(table_name)
-
-        return table_names
-
     def format_sql(self, created_tables, sql_query, file_names):
         if len(file_names) > 0:
-            table_names = self.load_tables_from_parquet(created_tables, file_names)
+            table_names = load_tables_from_parquet(self.db_connection, created_tables, file_names)
             sql_query = sql_query.format(*table_names)
         return sql_query
 
@@ -117,38 +88,9 @@ class IbisProducer:
             substrait_plan = json_format.MessageToJson(tpch_proto_bytes)
         return substrait_plan
 
-    def load_tables_from_parquet(
-        self,
-        created_tables: set,
-        file_names: Iterable[str],
-    ) -> list:
-        """
-        Load all the parquet files into separate tables in DuckDB.
-
-        Parameters:
-            created_tables:
-                The set of tables that have already been created.
-            file_names:
-                Name of parquet files.
-        Returns:
-            A list of the table names.
-        """
-        parquet_file_paths = SubstraitUtils.get_full_path(file_names)
-        table_names = []
-        for file_name, file_path in zip(file_names, parquet_file_paths):
-            table_name = Path(file_name).stem
-            table_name = table_name.translate(str.maketrans("", "", string.punctuation))
-            if table_name not in created_tables:
-                create_table_sql = f"CREATE TABLE {table_name} AS SELECT * FROM read_parquet('{file_path}');"
-                self.db_connection.execute(create_table_sql)
-                created_tables.add(table_name)
-            table_names.append(table_name)
-
-        return table_names
-
     def format_sql(self, created_tables, sql_query, file_names):
         if len(file_names) > 0:
-            table_names = self.load_tables_from_parquet(created_tables, file_names)
+            table_names = load_tables_from_parquet(self.db_connection, created_tables, file_names)
             sql_query = sql_query.format(*table_names)
         return sql_query
 
@@ -187,40 +129,43 @@ class IsthmusProducer:
 
         return substrait_plan_str
 
-    def load_tables_from_parquet(
-        self,
-        created_tables: set,
-        file_names: Iterable[str],
-    ) -> list:
-        """
-        Load all the parquet files into separate tables in DuckDB.
-
-        Parameters:
-            created_tables:
-                The set of tables that have already been created.
-            file_names:
-                Name of parquet files.
-        Returns:
-            A list of the table names.
-        """
-        parquet_file_paths = SubstraitUtils.get_full_path(file_names)
-        table_names = []
-        for file_name, file_path in zip(file_names, parquet_file_paths):
-            table_name = Path(file_name).stem
-            table_name = table_name.translate(str.maketrans("", "", string.punctuation))
-            if table_name not in created_tables:
-                create_table_sql = f"CREATE TABLE {table_name} AS SELECT * FROM read_parquet('{file_path}');"
-                self.db_connection.execute(create_table_sql)
-                created_tables.add(table_name)
-            table_names.append(table_name)
-
-        return table_names
-
     def format_sql(self, created_tables, sql_query, file_names):
         sql_query = sql_query.replace("'{}'", "{}")
         sql_query = sql_query.replace("'t'", "t")
         if len(file_names) > 0:
             self.file_names = file_names
-            table_names = self.load_tables_from_parquet(created_tables, file_names)
+            table_names = load_tables_from_parquet(self.db_connection, created_tables, file_names)
             sql_query = sql_query.format(*table_names)
         return sql_query
+
+
+def load_tables_from_parquet(
+    db_connection,
+    created_tables: set,
+    file_names: Iterable[str],
+) -> list:
+    """
+    Load all the parquet files into separate tables in DuckDB.
+
+    Parameters:
+        db_connection:
+            DuckDB Connection.
+        created_tables:
+            The set of tables that have already been created.
+        file_names:
+            Name of parquet files.
+    Returns:
+        A list of the table names.
+    """
+    parquet_file_paths = SubstraitUtils.get_full_path(file_names)
+    table_names = []
+    for file_name, file_path in zip(file_names, parquet_file_paths):
+        table_name = Path(file_name).stem
+        table_name = table_name.translate(str.maketrans("", "", string.punctuation))
+        if table_name not in created_tables:
+            create_table_sql = f"CREATE TABLE {table_name} AS SELECT * FROM read_parquet('{file_path}');"
+            db_connection.execute(create_table_sql)
+            created_tables.add(table_name)
+        table_names.append(table_name)
+
+    return table_names
