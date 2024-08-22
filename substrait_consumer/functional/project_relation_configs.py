e3b89945e374557aa140d642439b8e4d77936e29
Richard Tia <richie.tia@gmail.com>
2024-08-22 08:21:56 -0700

    feat: add projection relation tests

---
+++ substrait_consumer/functional/project_relation_configs.py
@@ -0,0 +1,47 @@
+from substrait_consumer.functional.queries.sql.relations.project_relations import (
+    PROJECT_RELATIONS)
+
+PROJECT_RELATION_TESTS = (
+    {
+        "test_name": "project_single_col",
+        "file_names": ["lineitem.parquet"],
+        "sql_query": PROJECT_RELATIONS["project_single_col"],
+        "ibis_expr": None
+    },
+    {
+        "test_name": "project_multi_col",
+        "file_names": ["lineitem.parquet"],
+        "sql_query": PROJECT_RELATIONS["project_multi_col"],
+        "ibis_expr": None
+    },
+    {
+        "test_name": "project_all_col",
+        "file_names": ["region.parquet"],
+        "sql_query": PROJECT_RELATIONS["project_all_col"],
+        "ibis_expr": None
+    },
+    {
+        "test_name": "extended_project",
+        "file_names": ["lineitem.parquet"],
+        "sql_query": PROJECT_RELATIONS["extended_project"],
+        "ibis_expr": None
+    },
+    {
+        "test_name": "subqeury_in_project",
+        "file_names": ["orders.parquet", "customer.parquet"],
+        "sql_query": PROJECT_RELATIONS["subqeury_in_project"],
+        "ibis_expr": None
+    },
+    {
+        "test_name": "distinct_in_project",
+        "file_names": ["lineitem.parquet"],
+        "sql_query": PROJECT_RELATIONS["distinct_in_project"],
+        "ibis_expr": None
+    },
+    {
+        "test_name": "count_distinct_in_project",
+        "file_names": ["lineitem.parquet"],
+        "sql_query": PROJECT_RELATIONS["count_distinct_in_project"],
+        "ibis_expr": None
+    },
+)
---
+++ substrait_consumer/functional/queries/sql/relations/project_relations.py
@@ -0,0 +1,72 @@
+from substrait_consumer.producers.duckdb_producer import DuckDBProducer
+from substrait_consumer.producers.datafusion_producer import DataFusionProducer
+
+PROJECT_RELATIONS = {
+    "project_single_col": (
+        """
+        SELECT L_DISCOUNT
+        FROM '{}'
+        ORDER BY L_DISCOUNT
+        LIMIT 20;
+        """,
+        [DuckDBProducer, DataFusionProducer],
+    ),
+    "project_multi_col": (
+        """
+        SELECT L_DISCOUNT, L_TAX
+        FROM '{}'
+        ORDER BY L_DISCOUNT, L_TAX
+        LIMIT 20;
+        """,
+        [DuckDBProducer, DataFusionProducer],
+    ),
+    "project_all_col": (
+        """
+        SELECT *
+        FROM '{}'
+        ORDER BY R_REGIONKEY, R_NAME, R_COMMENT
+        LIMIT 10;
+        """,
+        [DuckDBProducer, DataFusionProducer],
+    ),
+    "extended_project": (
+        """
+        SELECT L_QUANTITY, L_EXTENDEDPRICE*10 AS MULTI_PRICE
+        FROM '{}'
+        WHERE L_QUANTITY > 10
+        ORDER BY L_QUANTITY, MULTI_PRICE
+        LIMIT 10;
+        """,
+        [DuckDBProducer, DataFusionProducer],
+    ),
+    "subqeury_in_project": (
+        """
+        SELECT C_CUSTKEY,
+           (SELECT SUM(O_TOTALPRICE) 
+               FROM {}
+               WHERE C_CUSTKEY = O_CUSTKEY) 
+                 AS total_price
+           FROM {}
+           ORDER BY C_CUSTKEY
+           LIMIT 10;
+        """,
+        [DuckDBProducer, DataFusionProducer],
+    ),
+    "distinct_in_project": (
+        """
+        SELECT DISTINCT L_TAX
+        FROM '{}'
+        ORDER BY L_TAX
+        LIMIT 10;
+        """,
+        [DuckDBProducer, DataFusionProducer],
+    ),
+    "count_distinct_in_project": (
+        """
+        SELECT COUNT(DISTINCT L_EXTENDEDPRICE)
+        FROM '{}'
+        """,
+        [DuckDBProducer, DataFusionProducer],
+    ),
+
+}
