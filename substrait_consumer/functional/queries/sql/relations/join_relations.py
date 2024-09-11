f3c65eaac661ef7f8d3a3e53ad74a7da9a068b1c
Richard Tia <richie.tia@gmail.com>
2024-09-11 08:49:40 -0700

    fix: remove datafusion from single/mark join config

--- substrait_consumer/functional/queries/sql/relations/join_relations.py
+++ substrait_consumer/functional/queries/sql/relations/join_relations.py
@@ -157,7 +157,7 @@ JOIN_RELATIONS = {
             c1.C_NATIONKEY = c2.C_NATIONKEY
             AND c1.C_CUSTKEY <> c2.C_CUSTKEY;
         """,
-        [DuckDBProducer, DataFusionProducer, IsthmusProducer],
+        [DuckDBProducer, IsthmusProducer],
     ),
     "right_single_join": (
         """
@@ -176,7 +176,7 @@ JOIN_RELATIONS = {
             c1.C_NATIONKEY = c2.C_NATIONKEY
             AND c1.C_CUSTKEY <> c2.C_CUSTKEY;
         """,
-        [DuckDBProducer, DataFusionProducer, IsthmusProducer],
+        [DuckDBProducer, IsthmusProducer],
     ),
     "left_mark_join": (
         """
@@ -194,7 +194,7 @@ JOIN_RELATIONS = {
         FROM
             '{}' c;
         """,
-        [DuckDBProducer, DataFusionProducer, IsthmusProducer],
+        [DuckDBProducer, IsthmusProducer],
     ),
     "right_mark_join": (
         """
@@ -212,6 +212,6 @@ JOIN_RELATIONS = {
         FROM
             '{}' o;
         """,
-        [DuckDBProducer, DataFusionProducer, IsthmusProducer],
+        [DuckDBProducer, IsthmusProducer],
     ),
 }
