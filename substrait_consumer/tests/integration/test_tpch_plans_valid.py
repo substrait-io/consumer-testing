e09b0e803dfed38efe731fb5e66b0566ba87c494
Ingo Müller <ingomueller@google.com>
2024-12-17 09:05:35 +0000

    feat: use `producer` fixture in TPC-H plan tests
    This PR uses the `producer` fixture in the TPC-H generation and
    validation tests instead of manually constructing the `producer`
    instances with `DuckDBProducer()` and `IsthmusProducer()`. This adds the
    previously uncovered combination of generation and DuckDB.
    
    Signed-off-by: Ingo Müller <ingomueller@google.com>

--- substrait_consumer/tests/integration/test_tpch_plans_valid.py
+++ substrait_consumer/tests/integration/test_tpch_plans_valid.py
@@ -20,17 +20,17 @@ IDS = list((str(path[0]).removesuffix(".json") for path in TEST_CASE_PATHS))
 @pytest.mark.parametrize(["path"], TEST_CASE_PATHS, ids=IDS)
 @pytest.mark.usefixtures("prepare_tpch_parquet_data")
 @pytest.mark.produce_substrait_snapshot
-def test_isthmus_substrait_plan_generation(
+def test_substrait_plan_generation(
     path: Path,
     snapshot: Snapshot,
     record_property,
     db_con: duckdb.DuckDBPyConnection,
+    producer,
 ):
     test_case = load_json(CONFIG_DIR / path)
     local_files = test_case["local_files"]
     named_tables = test_case["named_tables"]
     sql_query = test_case["sql_query"]
-    producer = IsthmusProducer()
     substrait_producer_sql_test(
         path,
         snapshot,
@@ -46,45 +46,17 @@ def test_isthmus_substrait_plan_generation(
 
 @pytest.mark.parametrize(["path"], TEST_CASE_PATHS, ids=IDS)
 @pytest.mark.usefixtures("prepare_tpch_parquet_data")
-def test_isthmus_substrait_plans_valid(
+def test_substrait_plans_valid(
     path: Path,
     snapshot: Snapshot,
     record_property,
     db_con: duckdb.DuckDBPyConnection,
+    producer,
 ):
     test_case = load_json(CONFIG_DIR / path)
     local_files = test_case["local_files"]
     named_tables = test_case["named_tables"]
     sql_query = test_case["sql_query"]
-    producer = IsthmusProducer()
-    # Workaround to distinguish validity test from generation test:
-    path = Path(str(path).removesuffix(".json") + "-validate")
-    substrait_producer_sql_test(
-        path,
-        snapshot,
-        record_property,
-        db_con,
-        local_files,
-        named_tables,
-        sql_query,
-        producer,
-        validate=True,
-    )
-
-
-@pytest.mark.parametrize(["path"], TEST_CASE_PATHS, ids=IDS)
-@pytest.mark.usefixtures("prepare_tpch_parquet_data")
-def test_duckdb_substrait_plans_valid(
-    path: Path,
-    snapshot: Snapshot,
-    record_property,
-    db_con: duckdb.DuckDBPyConnection,
-):
-    test_case = load_json(CONFIG_DIR / path)
-    local_files = test_case["local_files"]
-    named_tables = test_case["named_tables"]
-    sql_query = test_case["sql_query"]
-    producer = DuckDBProducer()
     # Workaround to distinguish validity test from generation test:
     path = Path(str(path).removesuffix(".json") + "-validate")
     substrait_producer_sql_test(
