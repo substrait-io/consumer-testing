18f24f9b5991bcaccfdd519e8e965ca77bd1c4ce
Ingo Müller <ingomueller@google.com>
2024-12-17 08:53:48 +0000

    feat: express TPC-H validation tests with existing infra
    This PR uses the existing `substrait_producer_sql_test` test logic to
    implement the test for plan validity of the TPC-H queries. This leads to
    a moving of the outcome snapshots of these tests; however, the content
    of these files is unchanged. Since the validity tests use the same test
    definitions as the plan generation test targets and have, thus,
    conflicting snapshot sames with those tests, we slightly change the test
    name for the validity tests as a work-around, which leads to new plan
    snapshots.
    
    Signed-off-by: Ingo Müller <ingomueller@google.com>

--- substrait_consumer/tests/integration/test_tpch_plans_valid.py
+++ substrait_consumer/tests/integration/test_tpch_plans_valid.py
@@ -49,36 +49,27 @@ def test_isthmus_substrait_plan_generation(
 def test_isthmus_substrait_plans_valid(
     path: Path,
     snapshot: Snapshot,
+    record_property,
     db_con: duckdb.DuckDBPyConnection,
-) -> None:
-    """
-    Run the Isthmus generated substrait plans through the substrait validator.
-
-    Parameters:
-        substrait_query:
-            Substrait query.
-    """
+):
     test_case = load_json(CONFIG_DIR / path)
-    test_name = test_case["test_name"]
     local_files = test_case["local_files"]
     named_tables = test_case["named_tables"]
-    sql_query, supported_producers = test_case["sql_query"]
-    tpch_num = int(test_name.split("_")[-1])
-
-    assert "isthmus" in supported_producers
-
-    snapshot.snapshot_dir = snapshot.snapshot_dir.parent / f"test_tpch_sql_{tpch_num}"
-
+    sql_query = test_case["sql_query"]
     producer = IsthmusProducer()
-    producer.setup(db_con, local_files, named_tables)
-
-    try:
-        producer.produce_substrait(sql_query, validate=True)
-    except BaseException as e:
-        snapshot.assert_match(str(type(e)), f"test_tpch_sql_{tpch_num}_outcome.txt")
-        return
-
-    snapshot.assert_match("True", f"test_tpch_sql_{tpch_num}_outcome.txt")
+    # Workaround to distinguish validity test from generation test:
+    path = Path(str(path).removesuffix(".json") + "-validate")
+    substrait_producer_sql_test(
+        path,
+        snapshot,
+        record_property,
+        db_con,
+        local_files,
+        named_tables,
+        sql_query,
+        producer,
+        validate=True,
+    )
 
 
 @pytest.mark.parametrize(["path"], TEST_CASE_PATHS, ids=IDS)
@@ -86,38 +77,24 @@ def test_isthmus_substrait_plans_valid(
 def test_duckdb_substrait_plans_valid(
     path: Path,
     snapshot: Snapshot,
+    record_property,
     db_con: duckdb.DuckDBPyConnection,
-) -> None:
-    """
-    Run the Duckdb generated substrait plans through the substrait validator.
-
-    Parameters:
-        local_files:
-            A `dict` mapping format argument names to local files paths.
-        named_tables:
-            A `dict` mapping table names to local file paths.
-        sql_query:
-            SQL query.
-    """
+):
     test_case = load_json(CONFIG_DIR / path)
-    test_name = test_case["test_name"]
     local_files = test_case["local_files"]
     named_tables = test_case["named_tables"]
-    sql_query, supported_producers = test_case["sql_query"]
-    tpch_num = int(test_name.split("_")[-1])
-
-    assert "duckdb" in supported_producers
-
-    snapshot.snapshot_dir = snapshot.snapshot_dir.parent / f"test_tpch_sql_{tpch_num}"
-
-    # Format the sql query by inserting all the table names
-    duckdb_producer = DuckDBProducer()
-    duckdb_producer.setup(db_con, local_files, named_tables)
-
-    try:
-        duckdb_producer.produce_substrait(sql_query, validate=True)
-    except BaseException as e:
-        snapshot.assert_match(str(type(e)), f"test_tpch_sql_{tpch_num}_outcome.txt")
-        return
-
-    snapshot.assert_match("True", f"test_tpch_sql_{tpch_num}_outcome.txt")
+    sql_query = test_case["sql_query"]
+    producer = DuckDBProducer()
+    # Workaround to distinguish validity test from generation test:
+    path = Path(str(path).removesuffix(".json") + "-validate")
+    substrait_producer_sql_test(
+        path,
+        snapshot,
+        record_property,
+        db_con,
+        local_files,
+        named_tables,
+        sql_query,
+        producer,
+        validate=True,
+    )
