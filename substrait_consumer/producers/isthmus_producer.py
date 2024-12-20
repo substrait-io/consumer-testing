c7923dc5bdd374363fe959b357d75480c8cbc944
Ingo Müller <ingomueller@google.com>
2024-12-20 08:02:40 +0000

    feat: move Isthmus-specific code to `isthmus_producer.py`
    Some of the Isthmus-specific code lived in top-level files called
    `java_definitions.py` and `context.py`, which didn't convey a lot of
    meaning and suggested more general usage that what was actually
    happening. This PR moves the code directly to `isthmus_producer.py` (the
    only place where it is used) and or to the `producer` directory in a
    file `isthmus_java.py`, which indicates what it is used for.
    
    Signed-off-by: Ingo Müller <ingomueller@google.com>

--- substrait_consumer/producers/isthmus_producer.py
+++ substrait_consumer/producers/isthmus_producer.py
@@ -1,10 +1,15 @@
 import re
+from pathlib import Path
 
 import duckdb
-from .producer import SQLProducer, load_named_tables
 from ibis_substrait.compiler.core import SubstraitCompiler
+import jpype.imports
+import substrait_validator as sv
 
-from substrait_consumer.context import get_schema, produce_isthmus_substrait
+from .producer import SQLProducer, load_named_tables
+
+DATA_DIR = Path(__file__).parent.parent / "data"
+TPCH_SCHEMA_FILE = DATA_DIR / "tpch_parquet" / "schema.sql"
 
 
 class IsthmusProducer(SQLProducer):
@@ -46,11 +51,76 @@ class IsthmusProducer(SQLProducer):
         Returns:
             Substrait query plan in json format.
         """
-        schema_list = get_schema(self.table_names)
-        substrait_plan_str = produce_isthmus_substrait(sql_query, schema_list, validate)
+        schema_list = self._get_schema(self.table_names)
+        substrait_plan_str = self._produce_isthmus_substrait(
+            sql_query, schema_list, validate
+        )
 
         return substrait_plan_str
 
     def _format_sql(self, sql_query):
         sql_query = re.sub(r"'(\{[0-9a-zA-Z_]+\})'", r"\1", sql_query)
         return sql_query.replace("'t'", "t")
+
+    @staticmethod
+    def _produce_isthmus_substrait(sql_string, schema_list, validate=False):
+        """
+        Produce the substrait plan using Isthmus.
+
+        Parameters:
+            sql_string:
+                SQL query.
+            schema_list:
+                List of schemas.
+            validate:
+                Validate the Substrait plan.
+
+        Returns:
+            Substrait plan in json format.
+        """
+        from com.google.protobuf.util import JsonFormat as json_formatter
+
+        from . import isthmus_java as java
+
+        sql_to_substrait = java.SqlToSubstraitClass()
+        java_sql_string = jpype.java.lang.String(sql_string)
+        plan = sql_to_substrait.execute(java_sql_string, schema_list)
+        json_plan = json_formatter.printer().print_(plan)
+        if validate:
+            config = sv.Config()
+            config.override_diagnostic_level(1002, "info", "info")  # error
+            config.override_diagnostic_level(2001, "info", "info")  # warning
+            config.override_diagnostic_level(3005, "info", "info")  # warning
+            config.override_diagnostic_level(1, "info", "info")  # warning
+            sv.check_plan_valid(json_plan, config)
+        return json_plan
+
+    @staticmethod
+    def _get_schema(local_files):
+        """
+        Create the list of schemas based on the given file names.  If there are no files
+        give, a custom schema for the data is used.
+
+        Parameters:
+            local_files: List of file names.
+
+        Returns:
+            List of all schemas as a java list.
+        """
+        from . import isthmus_java as java
+
+        arr = java.ArrayListClass()
+        if local_files:
+            text_schema_file = open(TPCH_SCHEMA_FILE)
+            schema_string = text_schema_file.read().replace("\n", " ").split(";")[:-1]
+            for create_table in schema_string:
+                if "small" not in local_files[0]:
+                    create_table = create_table.replace("_small", "")
+                java_obj = jpype.JObject @ jpype.JString(create_table)
+                arr.add(java_obj)
+        java_obj = jpype.JObject @ jpype.JString(
+            "CREATE TABLE T(a integer, b integer, c boolean, d boolean)"
+        )
+        arr.add(java_obj)
+
+        return java.ListClass @ arr
